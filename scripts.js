90b6aa3c54505bdc28300724d6aa94e81b18b1bd
AshaThor <smartyparty101@hotmail.com>
2019-06-11 23:05:01 +0100

    Ghibly to Software
    Transitioned the bace code to software and away from ghibly

--- scripts.js
+++ scripts.js
@@ -10,25 +10,29 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://ashathor.github.io/sandbox/Json/testJson.json', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(codeLanguage => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = codeLanguage.name;
+
+      const h2 = document.createElement('h2')
+      h2.textContent = "Learnt: " + codeLanguage.learnt;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      codeLanguage.description = codeLanguage.summary.substring(0, 300);
+      p.textContent = `${codeLanguage.summary}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
+      card.appendChild(h2);
       card.appendChild(p);
     });
   } else {
